08b4994e79187e615ba6d2d3658a8135defd9c10
Johanna <joisk026@gmail.com>
2026-03-06 07:21:46 -0500

    Seguridad: Se eliminan tokens sensibles del código

--- sanity/lib/sanity.ts
+++ sanity/lib/sanity.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: 'zy6vgrm7', // Reemplaza con tu ID real
-  dataset: 'production',
+  // Usamos process.env para que Netlify inyecte el valor real de forma segura
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   useCdn: false, 
   apiVersion: '2023-05-03',
-  token: 'skpHMHn3AYDKgEMZp7pNTyGX8aDmdA7Mm1MT0lWvPWZ5JZBBpkjKjHThyqbILVJ8hBZN5qzuWrxWYFKa56WjMDoNBOjMUi5BcaybrPgm3LN6qXxxrTdwS7YIDtsl5MmhMyIxxgGScrZXmvgIkeBwP5yqs3aVt2L9vtttfWE5ZuZIVg7li5qo', // Vital para poder CREAR casos desde la web
+  // Aquí usamos el token de escritura que ya guardamos en Netlify
+  token: process.env.SANITY_API_WRITE_TOKEN, 
 })
